6b4fd8d500535b00f2eea8e9aae19be411f8153b
fushang318 <fushang318@gmail.com>
2012-08-20 11:50:25 +0800

    增加模型校验声明 和  模型的状态字段的状态字符串要声明成 模型常量

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -1,4 +1,9 @@
 class Milestone < ActiveRecord::Base
+  class State
+    CLOSED = 'CLOSED'
+    OPEN = 'OPEN'
+  end
+
   belongs_to :creator, :class_name => 'User', :foreign_key => :creator_id
   belongs_to :product, :class_name => 'Product', :foreign_key => :product_id
   
@@ -12,6 +17,10 @@ class Milestone < ActiveRecord::Base
 
 
   validates :product_id,  :presence => true
+  validates :creator_id,  :presence => true
+  validates :name,        :presence => true
+  validates :state,       :presence => true,
+    :inclusion => [Milestone::State::OPEN,Milestone::State::CLOSED]
 
 
   def create_report(user)
