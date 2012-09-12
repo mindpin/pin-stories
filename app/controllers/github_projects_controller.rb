f510d1a7e90e5c9d27339d32fde5dd3a45746838
mykingla <kingla_pei@163.com>
2012-09-12 10:34:58 +0800

    把普通的项目url转化为api url

--- app/controllers/github_projects_controller.rb
+++ app/controllers/github_projects_controller.rb
@@ -16,19 +16,25 @@ class GithubProjectsController < ApplicationController
     @last_sha, @first_sha = '', ''
     @last_sha = "?last_sha=" + params['last_sha'] if params['last_sha']
     @first_sha = "?first_sha=" + params['first_sha'] if params['first_sha']
- 
-    uri = URI.parse(ARGV[0] || @github_project.url + @last_sha + @first_sha)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true if uri.scheme == "https"  # enable SSL/TLS
+
+    uri = URI.parse(ARGV[0] || @github_project.url)
+    api_uri = "https://" + uri.host.gsub(/github.com/, "api.github.com/repos") + uri.path + "/commits"
+    api_uri = URI.parse(api_uri)
+
+
+    http = Net::HTTP.new(api_uri.host, api_uri.port)
+    http.use_ssl = true if api_uri.scheme == "https"  # enable SSL/TLS 
+
     http.start {
-      http.request_get(uri.path + @last_sha + @first_sha) {|res|
+      http.request_get(api_uri.path + @last_sha + @first_sha) {|res|
         # print res.body
         @commits = JSON.parse res.body
       }
     }
 
+    # 用于判断下一页
     @next_sha = "?last_sha=#{@commits[@commits.length - 1]['sha']}"
-    @next_path = uri.path + @next_sha
+    @next_path = api_uri.path + @next_sha
     http.start {
       http.request_get(@next_path) {|res|
         # print res.body
